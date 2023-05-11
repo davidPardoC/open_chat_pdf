63d7737b5ecbabd56711113d30c17adff90e0f2b
davidPardoC <pardodavid10@gmail.com>
2023-05-11 00:30:53 -0500

    Add file upload

--- server/routes/document.py
+++ server/routes/document.py
@@ -1,8 +1,13 @@
+from typing import Annotated
 from fastapi import APIRouter
+from fastapi import FastAPI, File, UploadFile
 
 router = APIRouter()
 
-@router.get(path="/", tags="document")
-async def post_document():
-    print("Hola Mundo")
-    return {"message":"Test document"}
+@router.post(path="/", tags="document")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+@router.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
