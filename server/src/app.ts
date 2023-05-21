0279ee7dcfb12f0939b7385b5393422940dc92c5
davidPardoC <pardodavid10@gmail.com>
2023-05-21 10:19:11 -0500

    Add file save feature

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { documentsRouter } from "./documents/documents.routes";
 import ErrorHandler from "./middlewares/error.middleware";
+import fileUpload from "express-fileupload"
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(fileUpload({createParentPath:true}))
 
 app.use("/documents", documentsRouter);
 
--- server/src/documents/documents.routes.ts
+++ server/src/documents/documents.routes.ts
@@ -1,8 +1,31 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { BadRequestExeption, CustomError } from "../error/custom.error";
+import { BadRequestExeption } from "../error/custom.error";
+import { UploadedFile } from "express-fileupload";
+import DocumentsController from "../controllers/documents.controller";
 
 export const documentsRouter = Router();
 
-documentsRouter.post("", (req: Request, res: Response, next: NextFunction) => {
-  throw BadRequestExeption();
-});
+documentsRouter.post(
+  "",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        
+      const file = req.files?.file as UploadedFile;
+      if (!file) {
+        throw BadRequestExeption("Please upload a file!");
+      }
+
+      if (!file.mimetype.includes("pdf")) {
+        throw BadRequestExeption("Only PDF files please!");
+      }
+
+      const response = await DocumentsController.uploadDocument(file);
+
+      res.send("ok");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+documentsRouter.get("", () => {});
