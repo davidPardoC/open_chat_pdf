376d42b068d0cc3fa3c594746021c0c0223a8868
David Pardo <david.pardo@rappi.com>
2023-07-17 19:51:26 -0500

    Add query param

--- web/src/pages/Library.tsx
+++ web/src/pages/Library.tsx
@@ -1,8 +1,16 @@
 import { Container } from "@chakra-ui/react";
 import LibraryHeader from "../components/LibraryHeader/LibraryHeader";
 import BooksSection from "../components/BooksSection/BooksSection";
+import { useSearchParams } from "react-router-dom";
 
 const Library = () => {
+  const [params] = useSearchParams();
+  const isEnabled = params.get("enabled");
+
+  if (isEnabled !== "antse") {
+    return <div></div>;
+  }
+
   return (
     <Container
       maxW={"container.lg"}
@@ -10,7 +18,7 @@ const Library = () => {
       backgroundColor={"transparent"}
     >
       <LibraryHeader />
-      <BooksSection/>
+      <BooksSection />
     </Container>
   );
 };
